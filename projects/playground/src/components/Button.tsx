460385699c1090db1ca3c2c9b6abb4c363b8b822
leventemathefc <levente.mathe@finnoconsult.at>
2019-07-18 15:04:59 +0200

    add button

---
+++ projects/playground/src/components/Button.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { theme } from './theme';
+
+export default function Button() {
+  return <button type="button">Click</button>;
+}
---
+++ projects/playground/src/theme/theme.js
@@ -0,0 +1,5 @@
+export const main = {
+  color: {
+    cta: '#ffcc00',
+  },
+};
