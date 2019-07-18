62b0e2fa7dd8a2364679b329f61588732b22122a
leventemathefc <levente.mathe@finnoconsult.at>
2019-07-18 16:15:14 +0200

    apply theme to a button

--- projects/playground/src/components/Button.tsx
+++ projects/playground/src/components/Button.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
 
-import { theme } from './theme';
+import { theme } from '../theme';
+
+const ButtonLayout = styled.button``;
 
 export default function Button() {
-  return <button type="button">Click</button>;
+  return (
+    <ThemeProvider theme={theme}>
+      <ButtonLayout>Click me!</ButtonLayout>
+    </ThemeProvider>
+  );
 }
--- projects/playground/src/theme/index.ts
+++ projects/playground/src/theme/index.ts
@@ -1,4 +1,4 @@
-export const main = {
+export const theme = {
   color: {
     cta: '#ffcc00',
   },
