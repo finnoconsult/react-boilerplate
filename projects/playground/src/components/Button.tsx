3a85f719b9b06382e8f447a87af8a98fead5a335
leventemathefc <levente.mathe@finnoconsult.at>
2019-07-18 16:56:43 +0200

    create basic button with theme styles

--- projects/playground/src/components/Button.tsx
+++ projects/playground/src/components/Button.tsx
@@ -3,12 +3,17 @@ import styled, { ThemeProvider } from 'styled-components';
 
 import { theme } from '../theme';
 
-const ButtonLayout = styled.button``;
+const ButtonStyles = styled.button`
+  background-color: ${props => props.theme.color.cta}
+  border: none;
+  border-radius: 4px;
+  font-size: ${props => props.theme.font.sizeButton}
+`;
 
 export default function Button() {
   return (
     <ThemeProvider theme={theme}>
-      <ButtonLayout>Click me!</ButtonLayout>
+      <ButtonStyles>Click me!</ButtonStyles>
     </ThemeProvider>
   );
 }
--- projects/playground/src/theme/index.ts
+++ projects/playground/src/theme/index.ts
@@ -2,4 +2,7 @@ export const theme = {
   color: {
     cta: '#ffcc00',
   },
+  font: {
+    sizeButton: '1.6rem',
+  },
 };
