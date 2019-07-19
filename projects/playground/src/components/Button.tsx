cfb713a87b8620871dfcbe4cfabbe4087a63799f
leventemathefc <levente.mathe@finnoconsult.at>
2019-07-19 10:03:07 +0200

    add conditional styling

--- projects/playground/src/components/Button.tsx
+++ projects/playground/src/components/Button.tsx
@@ -3,23 +3,31 @@ import styled from 'styled-components';
 
 import Text from './Text';
 
-const ButtonStyles = styled.button`
-  background-color: ${props => props.theme.color.cta};
-  border: none;
+interface ButtonStylesProps {
+  cta?: boolean;
+}
+
+const ButtonStyles = styled.button<ButtonStylesProps>`
+  background-color: ${props => (props.cta ? props.theme.color.cta : props.theme.color.background)};
+  border: ${props => (props.cta ? 'none' : `1px solid ${props.theme.color.text}`)};
   border-radius: 4px;
   pading: 14px auto;
+  cursor: pointer;
 `;
 
 const ButtonText = styled(Text)`
   font-size: ${props => props.theme.font.sizeButton};
+  font-weight: bold;
+  color: ${props => props.theme.color.text};
 `;
 
 interface Props {
   title?: string;
+  cta?: boolean;
 }
 
-export default ({ title }: Props) => (
-  <ButtonStyles>
+export default ({ title, cta }: Props) => (
+  <ButtonStyles cta={cta}>
     {title && (
       <ButtonText>{title}</ButtonText>
     )}
--- projects/playground/src/theme/index.ts
+++ projects/playground/src/theme/index.ts
@@ -1,6 +1,8 @@
 export const theme = {
   color: {
+    background: '#fff',
     cta: '#ffcc00',
+    text: '#191919',
   },
   font: {
     sizeButton: '1.6rem',
