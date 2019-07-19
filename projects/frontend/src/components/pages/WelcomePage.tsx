7618cb08e239f485ed833c523fbbc3343c759a7e
pitiboy <peter.horvath@finnoconsult.at>
2019-07-19 18:12:39 +0200

    not working, but not throwing error...

--- projects/frontend/src/components/pages/WelcomePage.tsx
+++ projects/frontend/src/components/pages/WelcomePage.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import styles from './WelcomePage.scss';
+
+console.log('styles', styles);
+
 const WelcomePage = () => (
-  <>mainpage</>
+  <div className="component">mainpage</div>
 );
 
 export default WelcomePage;
