e2451f4764486c4cd0a2761fcdfbdca2c09d5668
leventemathefc <levente.mathe@finnoconsult.at>
2019-07-22 17:26:02 +0200

    fix some layout issues

--- projects/playground/src/components/Divider.ts
+++ projects/playground/src/components/Divider.ts
@@ -5,8 +5,9 @@ interface DividerProps {
 }
 
 export default styled.div<DividerProps>`
-  padding: 0 ${props => props.fullWidth && '16px'};
+  margin: 0 ${props => !props.fullWidth && '16px'};
   height: 1px;
   width: 100%;
   background-color: ${props => props.theme.colors.divider};
+  overflow: hidden;
 `;
--- projects/playground/src/pages/Address.tsx
+++ projects/playground/src/pages/Address.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Page from '../components/Page';
+import SubPage from '../components/SubPage';
 import Title from '../components/Text/Title';
 import TextField, { MapPinUtilityView } from '../components/TextField';
 import Button from '../components/Button';
@@ -22,39 +23,41 @@ const InputLayout = styled.div`
 
 export default () => (
   <Page>
-    <InputLayout>
-      <Title>Ihre Adresse</Title>
-      <InputColumnLayout ratio="1fr">
-        <TextField badgeTitle="PLZ" utilityView={<MapPinUtilityView />} />
-      </InputColumnLayout>
-      <InputColumnLayout ratio="2fr 1fr">
-        <TextField badgeTitle="Strasse und Hausnummer" />
-        <TextField badgeTitle="Stockwerk" />
-      </InputColumnLayout>
-      <Divider />
+    <SubPage>
+      <InputLayout>
+        <Title>Ihre Adresse</Title>
+        <InputColumnLayout ratio="1fr">
+          <TextField badgeTitle="PLZ" utilityView={<MapPinUtilityView />} />
+        </InputColumnLayout>
+        <InputColumnLayout ratio="2fr 1fr">
+          <TextField badgeTitle="Strasse und Hausnummer" />
+          <TextField badgeTitle="Stockwerk" />
+        </InputColumnLayout>
+        <Divider fullWidth />
 
-      <Title>Wie können wir Sie erreichen?</Title>
-      <RadioGroup
-        title="Anrede"
-        name="gender"
-        items={[
-          { value: 'male', title: 'Herr' },
-          { value: 'female', title: 'Frau' },
-        ]}
+        <Title>Wie können wir Sie erreichen?</Title>
+        <RadioGroup
+          title="Anrede"
+          name="gender"
+          items={[
+            { value: 'male', title: 'Herr' },
+            { value: 'female', title: 'Frau' },
+          ]}
         // onChange={value => console.log(value)}
-      />
-      <InputColumnLayout ratio="1fr 1fr">
-        <TextField badgeTitle="Vorname" />
-        <TextField badgeTitle="Nachname" />
-      </InputColumnLayout>
-      <InputColumnLayout ratio="1fr 2fr">
-        <TextField badgeTitle="Vorwahl" />
-        <TextField badgeTitle="Handy-Nummer" />
-      </InputColumnLayout>
-    </InputLayout>
+        />
+        <InputColumnLayout ratio="1fr 1fr">
+          <TextField badgeTitle="Vorname" />
+          <TextField badgeTitle="Nachname" />
+        </InputColumnLayout>
+        <InputColumnLayout ratio="1fr 2fr">
+          <TextField badgeTitle="Vorwahl" />
+          <TextField badgeTitle="Handy-Nummer" />
+        </InputColumnLayout>
+      </InputLayout>
 
-    <FullWidthLayout>
-      <Button title="Handy-Nummer bestätigen" cta />
-    </FullWidthLayout>
+      <FullWidthLayout>
+        <Button title="Handy-Nummer bestätigen" cta />
+      </FullWidthLayout>
+    </SubPage>
   </Page>
 );
