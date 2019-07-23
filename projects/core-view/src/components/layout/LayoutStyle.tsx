2170f58a774e118e1e271f61788595aae8585061
pitiboy <peter.horvath@finnoconsult.at>
2019-07-23 08:34:09 +0200

    add overlay for landscape mode

--- projects/core-view/src/components/layout/LayoutStyle.tsx
+++ projects/core-view/src/components/layout/LayoutStyle.tsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
 import { Fragment, FragmentProps } from '../Fragment';
+import { Dimensions, Browser } from '../../types';
 
-const MobileLayoutStyle = styled(Fragment)<FragmentProps>`
+interface LayoutStyleProps extends FragmentProps{
+  browser?: Browser;
+}
+
+const MobileLayoutStyle = styled(Fragment)<LayoutStyleProps | Dimensions>`
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: 60px 1fr 50px;
@@ -37,6 +42,29 @@ const MobileLayoutStyle = styled(Fragment)<FragmentProps>`
   > aside.right {
     display: none;
   }
+
+  ${props => props.isLandscape && `
+    border: double 5px ${props.theme.colors.info};
+
+    &:after {
+      position: fixed;
+      z-index: 999999;
+      width: 100vw;
+      height: 100vh;
+      top: 0;
+      left: 0;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      content: 'Please rotate your device to Landscape mode!';
+      background-color: rgba(60,60,60,0.7);
+      color: ${props.theme.colors.info};
+      opacity: 0.9;
+    }
+
+  `}
 `;
 MobileLayoutStyle.displayName='LayoutStyle';
 
