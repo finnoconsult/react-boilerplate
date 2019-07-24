0a748bf7b5c4787b10ecb57492d41553fce08246
leventemathefc <levente.mathe@finnoconsult.at>
2019-07-24 17:40:50 +0200

    disable button in address done page if terms are not accepted

--- projects/playground/src/pages/AddressDone.tsx
+++ projects/playground/src/pages/AddressDone.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import HorizontalProgressView from '../components/HorizontalProgressView';
@@ -16,38 +16,42 @@ const AddressTextLayout = styled.div`
   margin-top: 5px;
 `;
 
-export default () => (
-  <Page>
-    <HorizontalProgressView
-      items={[
-        'Adresse',
-        'Handy-Nummer bestätigen',
-        'Beauftragen',
-      ]}
-      progress={2}
-    />
-    <SubPage>
-      <Title>Danke für Ihr Vertrauen, Herr Max Muster</Title>
-      <AddressTextLayout>
-        <Text>Einsatzadresse: </Text>
-        <Text>Landwehrstraße 67, 80331 München</Text>
-      </AddressTextLayout>
-    </SubPage>
-    <Offer />
-    <SubPage>
-      <CheckBoxGroup
-        name="agreement"
+export default () => {
+  const [termsAccepted, setTermsAccepted] = useState(false);
+
+  return (
+    <Page>
+      <HorizontalProgressView
         items={[
-          { title: 'Ich stimme den AGBs zu', value: 'agb' },
-          { title: 'Ich stimme den Richlinien gemäß der DSGVO zu', value: 'dsgvo' },
+          'Adresse',
+          'Handy-Nummer bestätigen',
+          'Beauftragen',
         ]}
-        // accepted={allAccepted => console.log(allAccepted)}
+        progress={2}
       />
-    </SubPage>
-    <SubPage>
-      <FullWidthLayout>
-        <Button disabled title="Auftrag bestätigen" cta />
-      </FullWidthLayout>
-    </SubPage>
-  </Page>
-);
+      <SubPage>
+        <Title>Danke für Ihr Vertrauen, Herr Max Muster</Title>
+        <AddressTextLayout>
+          <Text>Einsatzadresse: </Text>
+          <Text>Landwehrstraße 67, 80331 München</Text>
+        </AddressTextLayout>
+      </SubPage>
+      <Offer />
+      <SubPage>
+        <CheckBoxGroup
+          name="agreement"
+          items={[
+            { title: 'Ich stimme den AGBs zu', value: 'agb' },
+            { title: 'Ich stimme den Richlinien gemäß der DSGVO zu', value: 'dsgvo' },
+          ]}
+          accepted={allAccepted => setTermsAccepted(allAccepted)}
+        />
+      </SubPage>
+      <SubPage>
+        <FullWidthLayout>
+          <Button disabled={!termsAccepted} title="Auftrag bestätigen" cta />
+        </FullWidthLayout>
+      </SubPage>
+    </Page>
+  );
+};
