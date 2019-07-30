4d042a08ad5b60557fde916c6c584ec3befce1cd
pitiboy <peter.horvath@finnoconsult.at>
2019-07-30 08:56:58 +0200

    adjust design for mockupnotification

--- projects/frontend/src/components/layout/OverlayRouterContainer.tsx
+++ projects/frontend/src/components/layout/OverlayRouterContainer.tsx
@@ -1,54 +1,14 @@
 import React, { useContext } from 'react';
-import styled from 'styled-components';
 import { Route, Switch } from 'react-router-dom';
 import { observer } from 'mobx-react';
 
-import { FlexView, useLocation } from '@finnoconsult/core-view';
+import { useLocation, DialogContentStyle, DialogStyle } from '@finnoconsult/core-view';
 
 import StoreContext from '../../stores';
 
 import RateUs from '../overlays/RateUs';
 import ThankYou from '../overlays/ThankYou';
 import ComingSoon from '../overlays/ComingSoon';
-
-interface DialogType {
-  open?: boolean;
-  center?: boolean;
-}
-
-const DialogContentStyle = styled(FlexView)<DialogType>`
-  background-color: ${props => props.theme.colors.background};
-  min-height: 30vh;
-  flex: 0 0 auto;
-  justify-content: flex-start;
-  width: 100%;
-`;
-
-
-const DialogStyle = styled(FlexView).attrs(() => ({ as: 'dialog' })) <DialogType>`
-  position: fixed;
-  border: none;
-  background: none;
-  background: transparent;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: auto;
-
-
-  &[open] {
-    display: flex;
-    background: ${props => props.theme.colors.overlay};
-    height: 100vh;
-    height: ${props => `${props.theme.getDeviceHeight}px`};
-    z-index: 99;
-  }
-  &, &[open="false"] {
-    display: none;
-    z-index: 1;
-  }
-`;
-DialogStyle.displayName = 'DialogStyle';
 
 
 export default observer(() => {
